87baf7be0e30628919d1f592b6c18f6a8988b97f
JochenHansoul <jochenhansoul101@hotmail.com>
2020-12-21 17:19:32 +0100

    finished excercise

--- src/main/java/day05/BinaryBoardingWithNonExistingChairs.java
+++ src/main/java/day05/BinaryBoardingWithNonExistingChairs.java
@@ -12,6 +12,8 @@ Your seat wasn't at the very front or back, though; the seats with IDs +1 and -1
 What is the ID of your seat?
  */
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -33,14 +35,17 @@ public class BinaryBoardingWithNonExistingChairs {
 
         final int ROWS_LOG2 = (int) (Math.log(ROWS) / Math.log(2));
         final int COLUMNS_LOG2 = (int) (Math.log(COLUMNS) / Math.log(2));
+        final int LENGTH_SYMBOLS = ROWS_LOG2 + COLUMNS_LOG2;
 
         ArrayList<Integer> ids = new ArrayList<>();
+        ArrayList<Integer> binaryNumbers = new ArrayList<>();
         // read file
         try (BufferedReader reader = Files.newBufferedReader(PATH)) {
             String line;
             while ((line = reader.readLine()) != null) {
                 line = line.replaceAll("[" + UPPER_ROW_SYMBOL + UPPER_COLUMN_SYMBOL + "]", "1");
                 line = line.replaceAll("[" + LOWER_ROW_SYMBOL + LOWER_COLUMN_SYMBOL + "]", "0");
+                binaryNumbers.add(Integer.parseInt(line, 2));
 
                 int row = Integer.parseInt(line.substring(0, ROWS_LOG2), 2);
                 int column = Integer.parseInt(line.substring(ROWS_LOG2), 2);
@@ -50,7 +55,16 @@ public class BinaryBoardingWithNonExistingChairs {
             System.out.println(e.getMessage());
         }
         System.out.println("highest seat ID: " + Collections.max(ids));
-        // 871: to hight
-        // 864: right (added 'L' to UPPER_COLUMN_SYMBOL in stead of 'R'
+
+        for (int i =0; i < binaryNumbers.size(); i++) {
+            if (!binaryNumbers.contains(i)) {
+                String binaryString = Integer.toBinaryString(i);
+                binaryString = StringUtils.leftPad(binaryString, LENGTH_SYMBOLS, '0');
+                int row = Integer.parseInt(binaryString.substring(0, ROWS_LOG2), 2);
+                int column = Integer.parseInt(binaryString.substring(ROWS_LOG2), 2);
+                System.out.printf("doesn't contain %d, id: %d * 8 + %d = %d%n", i, row, column, row * 8 + column);
+            }
+        }
+        // apparantly 739 was the right answer
     }
 }
