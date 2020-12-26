4fe09e85bed83bd26401c38ca9c29f11f7244e57
JochenHansoul <jochenhansoul101@hotmail.com>
2020-12-26 03:44:08 +0100

    made a file so I could try out a few techniques to add bags to a list with some functions to update and print them

---
+++ src/main/java/day07/BagsTryout.java
@@ -0,0 +1,64 @@
+package day07;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BagsTryout {
+
+    private static Bag setAlreadyAddedBag(HashSet<Bag> bags, Bag alreadyAddedBag) {
+        Bag bag = null;
+        Bag[] arrayBags = bags.toArray(new Bag[0]);
+        int i = 0;
+        while (i < arrayBags.length) {
+            if (arrayBags[i].COLOR.equals(alreadyAddedBag.COLOR)) {
+                bag = arrayBags[i];
+                i = arrayBags.length;
+            }
+            i++;
+        }
+        return bag;
+    }
+
+    private static void printBags(Set<Bag> bags) {
+        for (Bag bag : bags) {
+            System.out.print(bag.COLOR + " {");
+            if (bag.getContent() != null) {
+                printBags(bag.getContent().keySet());
+            }
+            System.out.print("} ");
+        }
+    }
+
+    public static void main(String[] args) {
+        HashSet<Bag> bags = new HashSet<>();
+
+        // adding bags with the same color doesn't increase the amount of bags
+        HashMap<Bag, Integer> childBags = new HashMap<>();
+        childBags.put(new Bag("spotted red"), 10);
+        Bag darkRedBag = new Bag("dark red", childBags);
+        bags.add(darkRedBag);
+        bags.add(new Bag("dark green"));
+        bags.add(new Bag("dark blue")); // 3 bags added
+
+        // these 4 bags added are different to the bags that already have been added!!!
+        /*HashMap<Bag, Integer> bagsHashMap1 = new HashMap<>();
+        bagsHashMap1.put( new Bag("dark red"), 1);
+        bagsHashMap1.put(new Bag("dark green"), 2);
+        bagsHashMap1.put(new Bag("dark blue"), 2);
+        bags.add(new Bag("bright red", bagsHashMap1));*/
+
+        // adding bags content to a bag that is alreasy added (doesn't work)
+        Bag alreadyAddedBag = new Bag("dark red");
+        if (bags.contains(alreadyAddedBag)) {
+            alreadyAddedBag = setAlreadyAddedBag(bags, alreadyAddedBag);
+        }
+        childBags = new HashMap<>();
+        childBags.put( alreadyAddedBag, 1);
+        bags.add(new Bag("bright red", childBags));
+
+        // printing
+        System.out.println("amount : " + bags.size());
+        printBags(bags);
+    }
+}
