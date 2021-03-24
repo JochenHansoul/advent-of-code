b0b6994533367e5c923f0a4bfb37999dae44da07
JochenHansoul <jochenhansoul101@hotmail.com>
2021-03-24 17:41:59 +0100

    filled them with the appropriate values

--- src/main/java/day07/Colors.java
+++ src/main/java/day07/Colors.java
@@ -1,2 +1,22 @@
-package day07;public enum Colors {
+package day07;
+
+public enum Colors {
+    BRIGHT,
+    CLEAR,
+    DARK,
+    DIM,
+    DOTTED,
+    DRAB,
+    DULL,
+    FADED,
+    LIGHT,
+    MIRRORED,
+    MUTED,
+    PALE,
+    PLAID,
+    POSH,
+    SHINY,
+    STRIPED,
+    VIBRANT,
+    WAVY
 }
