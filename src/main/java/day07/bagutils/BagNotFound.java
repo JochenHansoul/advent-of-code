971b0ca374f142216a501daceac0fcb00f2d305d
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-02 23:52:19 +0200

    put the appropriate code into it

--- src/main/java/day07/bagutils/BagNotFound.java
+++ src/main/java/day07/bagutils/BagNotFound.java
@@ -1,2 +1,7 @@
-package day07.bagutils;public class BagNotFound {
+package day07.bagutils;
+
+public class BagNotFound extends Exception {
+    public BagNotFound(String s) {
+        super(s);
+    }
 }
