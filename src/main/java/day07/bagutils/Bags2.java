0ac575028d604c9de4528df609b076cebfb281e3
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-11 15:25:12 +0200

    changed methodname getOriginalBagOrCreateAndAddBag to addOrGetBag and removed some unnecessary things

--- src/main/java/day07/bagutils/Bags2.java
+++ src/main/java/day07/bagutils/Bags2.java
@@ -4,34 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class Bags2 {
-    HashSet<Bag> bags;
+    HashSet<Bag> bags = new HashSet<>();
 
     public Bags2(Path path) throws IOException {
         try (BufferedReader reader = Files.newBufferedReader(path)) {
-            this.bags = new HashSet<>();
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parentAndChildren = line.split(" bags contain ");
                 String[] parentBag = parentAndChildren[0]
                         .split(" ");
                 // current bag
-                Bag currentBag = getOriginalBagOrCreateAndAddNewBag(
+                Bag currentBag = addOrGetBag(
                         Pattern.valueOf(parentBag[0].toUpperCase()),
                         Color.valueOf(parentBag[1].toUpperCase()));
                 // child bags
                 if (!parentAndChildren[1].equals("no other bags.")) {
-                    HashMap<Bag, Integer> currentBagContent = new HashMap<>(); // don't forget to create a new object!
                     String[] childBags = parentAndChildren[1]
                             .substring(0, parentAndChildren[1].length() - 1) // removing last "."
                             .replaceAll(" bag[s]", "")
                             .split(", ");
                     for (String childBagString : childBags) {
                         String[] childBagArray = childBagString.split(" ");
-                        Bag childBag = getOriginalBagOrCreateAndAddNewBag(
+                        Bag childBag = addOrGetBag(
                                 Pattern.valueOf(childBagArray[1].toUpperCase()),
                                 Color.valueOf(childBagArray[2].toUpperCase()));
                         currentBag.content.put(childBag, Integer.parseInt(childBagArray[0]));
@@ -47,7 +44,7 @@ public class Bags2 {
         return getOriginalBag(new Bag(pattern, color));
     }
 
-    private Bag getOriginalBagOrCreateAndAddNewBag(Pattern pattern, Color color) {
+    private Bag addOrGetBag(Pattern pattern, Color color) {
         Bag bag = new Bag(pattern, color);
         if (this.bags.contains(bag)) {
             bag = getOriginalBag(bag);
