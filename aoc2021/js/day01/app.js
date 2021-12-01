12e8f44d5ae9dd5d01e42f066b278366b467e49c
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-02 00:32:35 +0100

    Converted the array of strings to integers and put the code into for calculating into a function

--- aoc2021/js/day01/app.js
+++ aoc2021/js/day01/app.js
@@ -3,14 +3,21 @@
 const fs = require("fs");
 const path = "../../resources/day01/input.txt";
 
-let lines = fs.readFileSync(path,"utf8").split("\n");
-let counter = 0;
-let previousNumber = parseInt(lines.shift());
-for (let line of lines) {
-    let n = parseInt(line);
-    if (previousNumber < n) {
-        counter++;
+const increasedAmount = numbers => {
+    let counter = 0;
+    let previousNumber = numbers.shift();
+    for (let n of numbers) {
+        if (previousNumber < n) {
+            counter++;
+        }
+        previousNumber = n;
     }
-    previousNumber = n;
-}
-console.log(counter);
+    return counter;
+};
+
+let lines = fs.readFileSync(path,"utf8").split("\n");
+let numbers = lines.map(function (x) { 
+    return parseInt(x, 10); 
+});
+
+console.log(increasedAmount(numbers));
