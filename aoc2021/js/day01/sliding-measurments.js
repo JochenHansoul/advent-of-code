a106be3971ddfc6e3076e941b6906c5e7fc1f8da
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-02 00:54:50 +0100

    Prepared start of second part

---
+++ aoc2021/js/day01/sliding-measurments.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const fs = require("fs");
+const path = "../../resources/day01/input.txt";
+
+const increasedAmount = numbers => {
+    let counter = 0;
+    let previousNumber = numbers.shift();
+    for (let n of numbers) {
+        if (previousNumber < n) {
+            counter++;
+        }
+        previousNumber = n;
+    }
+    return counter;
+};
+
+let numbers = fs.readFileSync(path,"utf8")
+    .split("\n")
+    .map((x) => parseInt(x, 10));
+
+let result = increasedAmount(numbers)
+console.log(result);
