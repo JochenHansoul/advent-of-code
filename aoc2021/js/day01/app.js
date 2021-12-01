1f9d595275f9b3be87c586f316c692fdd650c35f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-02 00:03:25 +0100

    Finished first part of day 1

--- aoc2021/js/day01/app.js
+++ aoc2021/js/day01/app.js
@@ -5,7 +5,17 @@ const path = "../../resources/day01/input.txt"
 
 fs.readFile(path, (err, data) => {
     if (err === null) {
-        console.log(data.toString());
+        let array = data.toString().split("\n");
+        let counter = 0;
+        let previousNumber = parseInt(array.shift());
+        for (let s of array) {
+            let n = parseInt(s);
+            if (previousNumber < n) {
+                counter++;
+            }
+            previousNumber = n;
+        }
+        console.log(counter);
     } else {
         throw err;
     }
