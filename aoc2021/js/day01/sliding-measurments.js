e800050bdc4736fe980ec5096feb1f442039fa2a
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-02 01:04:47 +0100

    Finished part two

--- aoc2021/js/day01/sliding-measurments.js
+++ aoc2021/js/day01/sliding-measurments.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const path = "../../resources/day01/input.txt";
+//const path = "../../resources/day01/example.txt";
 
 const increasedAmount = numbers => {
     let counter = 0;
@@ -15,9 +16,18 @@ const increasedAmount = numbers => {
     return counter;
 };
 
+const slidingWindow = numbers => {
+    let result = [];
+    for (let i = 0; i < numbers.length - 2; i++) {
+        result.push(numbers[i] + numbers[i + 1] + numbers[i + 2]);
+    }
+    return result;
+};
+
 let numbers = fs.readFileSync(path,"utf8")
     .split("\n")
     .map((x) => parseInt(x, 10));
 
-let result = increasedAmount(numbers)
+let slidingNumbers = slidingWindow(numbers);
+let result = increasedAmount(slidingNumbers)
 console.log(result);
