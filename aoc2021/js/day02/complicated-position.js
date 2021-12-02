6b3a64bd905b8a40727711a959a6d2bd903942b1
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-02 22:29:48 +0100

    Added all code into its own function

--- aoc2021/js/day02/complicated-position.js
+++ aoc2021/js/day02/complicated-position.js
@@ -3,16 +3,23 @@
 const fs = require("fs");
 const path = "../../resources/day02/input.txt";
 
-let instructions = fs.readFileSync(path,"utf8")
-    .split("\n")
-    .map((x) => x.split(" "))
-    .map((array) => {
-        return {
-            action: array[0],
-            amount: parseInt(array[1])
-        }
-    });
-instructions.pop();
+const readFile = (fs, path) => {
+    let instructions = fs.readFileSync(path,"utf8")
+        .split("\n")
+    instructions.pop();
+    return instructions;
+};
+
+const getInstructions = array => {
+    return array
+        .map((x) => x.split(" "))
+        .map((array) => {
+            return {
+                action: array[0],
+                amount: parseInt(array[1])
+            }
+        });
+};
 
 /*
 down X increases your aim by X units.
@@ -24,17 +31,27 @@ forward X does two things:
 Again note that since you're on a submarine, down and up do the opposite of
 what you might expect: "down" means aiming in the positive direction.
 */
-let aim = 0;
-let horizontalPosition = 0;
-let depth = 0;
-for (let instruction of instructions) {
-    if (instruction.action === "down") {
-        aim += instruction.amount;
-    } else if (instruction.action === "up") {
-        aim -= instruction.amount;
-    } else {
-        horizontalPosition += instruction.amount;
-        depth += aim * instruction.amount;
+const getPosition = instructions => {
+    let aim = 0;
+    let horizontalPosition = 0;
+    let depth = 0;
+    for (let instruction of instructions) {
+        if (instruction.action === "down") {
+            aim += instruction.amount;
+        } else if (instruction.action === "up") {
+            aim -= instruction.amount;
+        } else {
+            horizontalPosition += instruction.amount;
+            depth += aim * instruction.amount;
+        }
     }
-}
-console.log(horizontalPosition * depth);
+    return {
+        horizontalPosition: horizontalPosition,
+        depth: depth
+    }
+};
+
+// start code
+let instructions = getInstructions(readFile(fs, path));
+let position = getPosition(instructions);
+console.log(position.horizontalPosition * position.depth);
