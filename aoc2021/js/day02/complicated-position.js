92880064a88b08a61cead5a5c66a9fed27a8cc4e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-02 21:53:42 +0100

    Completed second part of day two

--- aoc2021/js/day02/complicated-position.js
+++ aoc2021/js/day02/complicated-position.js
@@ -14,3 +14,27 @@ let instructions = fs.readFileSync(path,"utf8")
     });
 instructions.pop();
 
+/*
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+    It increases your horizontal position by X units.
+    It increases your depth by your aim multiplied by X.
+
+Again note that since you're on a submarine, down and up do the opposite of
+what you might expect: "down" means aiming in the positive direction.
+*/
+let aim = 0;
+let horizontalPosition = 0;
+let depth = 0;
+for (let instruction of instructions) {
+    if (instruction.action === "down") {
+        aim += instruction.amount;
+    } else if (instruction.action === "up") {
+        aim -= instruction.amount;
+    } else {
+        horizontalPosition += instruction.amount;
+        depth += aim * instruction.amount;
+    }
+}
+console.log(horizontalPosition * depth);
