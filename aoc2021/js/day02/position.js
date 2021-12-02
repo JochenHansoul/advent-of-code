4fd0fe7b73ed45e3d968f92e1f6bb0da86db8480
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-02 21:36:14 +0100

    finished first part

--- aoc2021/js/day02/position.js
+++ aoc2021/js/day02/position.js
@@ -3,6 +3,26 @@
 const fs = require("fs");
 const path = "../../resources/day02/input.txt";
 
-let lines = fs.readFileSync(path,"utf8")
-    .split("\n");
+let instructions = fs.readFileSync(path,"utf8")
+    .split("\n")
+    .map((x) => x.split(" "))
+    .map((array) => {
+        return {
+            action: array[0],
+            amount: parseInt(array[1])
+        }
+    });
+instructions.pop();
 
+let horizontalPosition = 0;
+let depth = 0;
+for (let instruction of instructions) {
+    if (instruction.action === "forward") {
+        horizontalPosition += instruction.amount;
+    } else if (instruction.action === "up") {
+        depth -= instruction.amount;
+    } else {
+        depth += instruction.amount;
+    }
+}
+console.log(horizontalPosition * depth);
