e7dae7974739c4935c3334850bb1fc56c0df6729
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-04 19:22:03 +0100

    Managed to change the raw string input to number input

--- aoc2021/js/day04/bingo.js
+++ aoc2021/js/day04/bingo.js
@@ -10,5 +10,29 @@ const readFile = (fs, path) => {
     return instructions;
 };
 
+const getBingo = stringBingo => {
+    return stringBingo.map((x) => {
+            return x.trim().replace(/  /g, " ")
+                .split(" ")
+                .map((y) => parseInt(y, 10));
+        });
+};
+
+const getBingos = (array, vertical) => {
+    const output = [];
+    output.push(getBingo(array.splice(0, vertical)));
+    while (array.length > 0) {
+        array.shift();
+        output.push(getBingo(array.splice(0, vertical)));
+    }
+    return output;
+};
+
 let lines = readFile(fs, path);
-console.log(lines);
+let numbers = lines.shift()
+    .split(",")
+    .map((x) => parseInt(x, 10));
+lines.shift();
+
+let bingos = getBingos(lines, 5);
+console.log(bingos);
