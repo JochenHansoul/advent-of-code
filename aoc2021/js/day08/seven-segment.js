9e03cb716ac3307313a2b6918f2aa3c4d35d1d65
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-09 00:32:27 +0100

    Managed to process the input to understandable objects

--- aoc2021/js/day08/seven-segment.js
+++ aoc2021/js/day08/seven-segment.js
@@ -9,5 +9,26 @@ const readFile = (fs, path) => {
         .slice(0, -1);
 };
 
-const lines = readFile(fs, path);
+const lines = readFile(fs, path)
+    .map((x) => {
+        const array = x.split(" | ");
+        return {
+            pattern: array[0].split(/ /g).map((x) => x.length),
+            output: array[1].split(/ /g).map((x) => x.length)
+        };
+    });
+/*
+input:
+ten unique signal patterns, a | delimiter, and finally the four digit output value
+
+each use a unique number of segments:
+1   2
+4   4
+7   3
+8   7
+
+question:
+In the output values, how many times do digits 1, 4, 7, or 8 appear?
+*/
+
 console.log(lines);
