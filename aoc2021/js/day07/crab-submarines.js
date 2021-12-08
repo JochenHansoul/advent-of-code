0d79e2b1dbd70c17c0661d13176d9476184e3d77
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-08 01:00:54 +0100

    Completed part 1 of day 7

--- aoc2021/js/day07/crab-submarines.js
+++ aoc2021/js/day07/crab-submarines.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const fs = require("fs");
-const path = "../../resources/day07/example.txt";
+const path = "../../resources/day07/input.txt";
 
 const readFile = (fs, path) => {
     return fs.readFileSync(path, "utf8")
@@ -9,8 +9,27 @@ const readFile = (fs, path) => {
         .slice(0, -1);
 };
 
+const getDistance = (positions, destination) => {
+    let distance = 0;
+    for (let position of positions) {
+        distance += Math.abs(position - destination);
+    }
+    return distance;
+};
+
 
 // start code
 const submarines = readFile(fs, path)[0].split(/,/g)
     .map((x) => parseInt(x, 10));
-console.log(submarines);
+//const sum = submarines.reduce((a, b) => a + b);
+//const average = Math.round(sum / submarines.length);
+let counter = 0;
+let previousNumber = getDistance(submarines, counter) + 1;
+let number = getDistance(submarines, counter);
+while (number < previousNumber) {
+    previousNumber = number;
+    number = getDistance(submarines, ++counter);
+}
+console.log(previousNumber);
+
+// 353240 (too high)
