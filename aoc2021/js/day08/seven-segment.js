0a68952e05ea2083d09926d2b668be28dba5719d
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-09 01:02:06 +0100

    Finished part 1 of day 8

--- aoc2021/js/day08/seven-segment.js
+++ aoc2021/js/day08/seven-segment.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const fs = require("fs");
-const path = "../../resources/day08/example.txt";
+const path = "../../resources/day08/input.txt";
+
+const searchedNumbers = [2, 4, 3, 7];
 
 const readFile = (fs, path) => {
     return fs.readFileSync(path, "utf8")
@@ -9,14 +11,24 @@ const readFile = (fs, path) => {
         .slice(0, -1);
 };
 
-const lines = readFile(fs, path)
-    .map((x) => {
-        const array = x.split(" | ");
-        return {
-            pattern: array[0].split(/ /g).map((x) => x.length),
-            output: array[1].split(/ /g).map((x) => x.length)
-        };
-    });
+const includedNumbers = (numbers, searchedNumbers) => {
+    let counter = 0;
+    for (const n of numbers) {
+        if (searchedNumbers.includes(n)) {
+            counter++;
+        }
+    }
+    return counter;
+};
+
+const includedNumbersOfMatrix = (matrix, searchedNumbers) => {
+    let result = 0;
+    for (let numbers of matrix) {
+        result += includedNumbers(numbers, searchedNumbers);
+    }
+    return result;
+}
+
 /*
 input:
 ten unique signal patterns, a | delimiter, and finally the four digit output value
@@ -31,4 +43,17 @@ question:
 In the output values, how many times do digits 1, 4, 7, or 8 appear?
 */
 
-console.log(lines);
+// start code
+const input = readFile(fs, path)
+    .map((x) => {
+        const array = x.split(" | ");
+        return {
+            pattern: array[0].split(/ /g).map((x) => x.length),
+            output: array[1].split(/ /g).map((x) => x.length)
+        };
+    });
+
+const outputNumbers = input.map((x) => x.output);
+//console.log("searched numbers:", searchedNumbers);
+//console.log(outputNumbers);
+console.log(includedNumbersOfMatrix(outputNumbers, searchedNumbers));
