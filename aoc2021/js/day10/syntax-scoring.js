e0aeb380c6f39628d93a4c6ed27ed0bc0ab0426d
JochenHansoul <jochenhansoul101@hotmail.com>
2022-01-06 00:21:08 +0100

    Some try to solve day 10 (made on the day but with little effort or time)

--- aoc2021/js/day10/syntax-scoring.js
+++ aoc2021/js/day10/syntax-scoring.js
@@ -3,12 +3,59 @@
 const fs = require("fs");
 const path = "../../resources/day10/example.txt";
 
+const pairs = [
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+    ["<", ">"]
+];
+
 const readFile = (fs, path) => {
     return fs.readFileSync(path, "utf8")
         .split("\n")
         .slice(0, -1);
 };
 
+const getCorruptedSymbol = line => {
+    // ()()()<]
+    // ()
+    let difference = (line.length - line.replace(/\(/g, "").length)
+        - (line.length - line.replace(/\)/g, "").length);
+    console.log(difference);
+    if (difference === -1) {
+        return "]";
+    }
+    // []
+    difference = (line.length - line.replace(/\[/g, "").length)
+        - (line.length - line.replace(/\]/g, "").length);
+    if (difference === -1) {
+        return "]";
+    }
+    // {}
+    difference = (line.length - line.replace(/\{/g, "").length)
+        - (line.length - line.replace(/\}/g, "").length);
+    if (difference === -1) {
+        return "]";
+    }
+    // <>
+    difference = (line.length - line.replace(/\</g, "").length)
+        - (line.length - line.replace(/\>/g, "").length);
+    if (difference === -1) {
+        return "]";
+    }
+    return "";
+};
+
+const getCorruptedSymbols = lines => {
+    const symbols = [];
+    for (const line of lines) {
+        symbols.push(getCorruptedSymbol(line));
+    }
+    return symbols;
+};
+
 // start code
 const input = readFile(fs, path);
-console.log(input);
+//console.log(input);
+const s = getCorruptedSymbols(input);
+console.log(s);
